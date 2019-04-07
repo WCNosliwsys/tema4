63eac870f94c7891e954f211cfb4443a1dc62f79
Ing Wilson <nosliwsys@gmail.com>
2019-04-07 16:59:47 -0500

    fin

---
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/test_4/ReceptorSMS.java
@@ -0,0 +1,13 @@
+package com.a952000243.ingwilson.nosliwsys.test_4;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ReceptorSMS extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, Servicio.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/test_4/Servicio.java
@@ -0,0 +1,34 @@
+package com.a952000243.ingwilson.nosliwsys.test_4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Servicio extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        setContentView(R.layout.activity_servicio);
+        setContentView(R.layout.activity_servicio);
+        Button arrancar = (Button) findViewById(R.id.boton_arrancar);
+        arrancar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startService(new Intent(Servicio.this,
+                        ServicioMusica.class));
+            }
+        });
+        Button detener = (Button) findViewById(R.id.boton_detener);
+        detener.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                stopService(new Intent(Servicio.this,
+                        ServicioMusica.class));
+            }
+        });
+    }
+}
